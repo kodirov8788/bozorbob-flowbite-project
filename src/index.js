1677829b8e5f72bd09a820ff2fbdea9df04b52e1
yosinbek <muxtasarabduxalilova8@gmail.com>
2023-11-28 17:11:25 +0500

    yosinxon merge

--- src/index.js
+++ src/index.js
@@ -11,16 +11,18 @@ import Navbar from './components/Navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Provider store={store}>
+
+    <BrowserRouter>
       <Navbar />
 
         <Routes>
           <Route path='/' element={<App />} />
           <Route path='/dashboard' element={<Dashboard/>}/>
         </Routes>
-      </Provider>
     </BrowserRouter>
+    </Provider>
+
   </React.StrictMode>
 );
 
